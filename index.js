d0ea31317fda7883200291b4d9a9176f52565c8f
pranik chainani <pranikc@gmail.com>
2021-08-13 10:39:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 62
+const num2 = 1
+const multiply = num1 * num2
 
+const random = Math.ceil(Math.random()) * 10
+
+const num3 = 9
+const num4 = 5
+const mod = num3 % num4
+
+const max = Math.max(20, 1, 12)
 // Write your code here
